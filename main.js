98b0f68d141a34dab3e6b1b1ab60024504c379e4
aarón montoya-moraga <montoyamoraga@gmail.com>
2024-03-13 13:29:23 -0300

    comentar ejemplo

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((response) => response.json())
     .then((json) => {
         console.log(json);
-                    //You can play here doing a loop and using de iterator "i" for make it at one.
+            //You can play here doing a loop and using de iterator "i" for make it at one.
             //Example: document.querySelector(".faq--0" + i + "h1").innerHTML = json[0].title;
             //Example: document.querySelector(".faq--0" + i + " p").innerHTML = json[0].answer;
-            document.querySelector(".faq--01 h1").innerHTML = json[0].title;
-            document.querySelector(".faq--01 p").innerHTML = json[0].answer;
+            //document.querySelector(".faq--01 h1").innerHTML = json[0].title;
+            //document.querySelector(".faq--01 p").innerHTML = json[0].answer;
 
     })
 });
