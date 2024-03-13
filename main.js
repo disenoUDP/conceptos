30ff54a3638f699e56351e284c8b546405dd5733
aarón montoya-moraga <montoyamoraga@gmail.com>
2024-03-13 15:23:50 -0300

    agregar logica para encabezados de tabla

--- main.js
+++ main.js
@@ -6,21 +6,21 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch('./conceptos.json')
     .then((response) => response.json())
     .then((json) => {
-        console.log(json);
         let data = json.conceptos;
-        console.log(data);
+        let tabla = document.appendChild(document.createElement('table'));
+        document.createElement('h1').innerHTML = 'Conceptos';
+
+        // crear encabezados
+        tabla.appendChild(document.createElement('th')).innerHTML = 'Nombre';
+        tabla.appendChild(document.createElement('th')).innerHTML = 'Definición';
+
         for (let i = 0; i < data.length; i++) {
             console.log(data[i]);
-            conceptosDiv.appendChild(document.createElement('h2')).innerHTML = data[i].nombre;
-           conceptosDiv.appendChild(document.createElement('p')).innerHTML = data[i].definicion;
+            // crear nueva fila
+            let fila = tabla.appendChild(document.createElement('tr'));
+
+            tabla.appendChild(document.createElement('td')).innerHTML = data[i].nombre;
+            tabla.appendChild(document.createElement('td')).innerHTML = data[i].definicion;
         }
-       
-
-            //You can play here doing a loop and using de iterator "i" for make it at one.
-            //Example: document.querySelector(".faq--0" + i + "h1").innerHTML = json[0].title;
-            //Example: document.querySelector(".faq--0" + i + " p").innerHTML = json[0].answer;
-            //document.querySelector(".faq--01 h1").innerHTML = json[0].title;
-            //document.querySelector(".faq--01 p").innerHTML = json[0].answer;
-
     })
 });
