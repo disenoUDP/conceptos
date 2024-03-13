e5999bbe142617d19cbdce751356e3245c1e1f6a
aarón montoya-moraga <montoyamoraga@gmail.com>
2024-03-13 13:32:49 -0300

    agregar bucle

--- main.js
+++ main.js
@@ -6,7 +6,13 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch('./conceptos.json')
     .then((response) => response.json())
     .then((json) => {
-        console.log(json);
+        // console.log(json);
+        for (let i = 0; i < json.length; i++) {
+            conceptosDiv.appendChild(document.createElement('h2')).innerHTML = json[i].nombre;
+            conceptosDiv.appendChild(document.createElement('p')).innerHTML = json[i].definicion;
+        }
+       
+
             //You can play here doing a loop and using de iterator "i" for make it at one.
             //Example: document.querySelector(".faq--0" + i + "h1").innerHTML = json[0].title;
             //Example: document.querySelector(".faq--0" + i + " p").innerHTML = json[0].answer;
