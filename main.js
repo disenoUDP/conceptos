b557528061a17a13d3405bd1eb80f268676a7369
aarón montoya-moraga <montoyamoraga@gmail.com>
2024-03-13 13:28:46 -0300

    probar console log de json

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-console.log('Hello World');
 
 // tomar division
 const conceptosDiv = document.getElementById('conceptos');
 
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('./conceptos.json')
+    .then((response) => response.json())
+    .then((json) => {
+        console.log(json);
+                    //You can play here doing a loop and using de iterator "i" for make it at one.
+            //Example: document.querySelector(".faq--0" + i + "h1").innerHTML = json[0].title;
+            //Example: document.querySelector(".faq--0" + i + " p").innerHTML = json[0].answer;
+            document.querySelector(".faq--01 h1").innerHTML = json[0].title;
+            document.querySelector(".faq--01 p").innerHTML = json[0].answer;
 
-conceptosDiv.appendChild(document.createElement('br'));
+    })
+});
